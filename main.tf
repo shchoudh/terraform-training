622d3f00cc6f4af5360c160e494af636583506e6
Shwetha Choudhary <shchoudh@akamai.com>
2025-10-15 17:23:52 -0400

    Data Sources excercise

---
+++ main.tf
@@ -0,0 +1,24 @@
+data "akamai_group" "my_group" {
+    group_name = "ShChoudhary"
+    contract_id = "ctr_1-1NC95D"
+}
+
+output "my_group" {
+    value = data.akamai_group.my_group.id
+}
+
+data "akamai_appsec_configuration" "my_appsec_config" {
+    name = "shchoudh_waf_sc1"
+}
+
+output "my_appsec_config" {
+    value = data.akamai_appsec_configuration.my_appsec_config.id
+}
+
+data "akamai_property" "my_property" {
+    name = "test-amd2"
+}
+
+output "my_property" {
+    value = data.akamai_property.my_property.product_id
+}
